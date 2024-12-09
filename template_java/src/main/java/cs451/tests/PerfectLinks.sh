b6fe05ee3a4544998b5b2be8943d6da4cf1be4be
Léonard Amsler <leonard.amsler@gmail.com>
2024-12-09 12:03:39 +0100

    Milestone 2 version

--- template_java/src/main/java/cs451/tests/PerfectLinks.sh
+++ template_java/src/main/java/cs451/tests/PerfectLinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly BASE_PATH="/home/dcl/Desktop/CS451-2024-project/template_java/"
+readonly BASE_PATH="/Users/leonardamsler/Desktop/EPFL/Master/MA1/Distributed_algorithms/Project/CS451-2024-project/template_java"
 readonly RUN_PATH="run.sh"
 readonly BUILD_PATH="build.sh"
 readonly TC_PATH="../tools/tc.py"
